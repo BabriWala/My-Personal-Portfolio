5c88009c214e0168026837b755c294a037d69853
BabriWala <hanzalafarhann@gmail.com>
2023-02-10 20:08:08 +0600

    Hero Section Added

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -1,30 +1,37 @@
+import Image from 'next/image';
+import banner from '../../assets/banner-picture.jpg'
+import Link from 'next/link';
+
 const Hero = () => {
   return (
     <section className="text-gray-600 body-font">
       <div className="container mx-auto flex px-5 py-24 items-center justify-center flex-col">
-        <img
+        {/* <img
           className="lg:w-2/6 md:w-3/6 w-5/6 mb-10 object-cover object-center rounded"
           alt="hero"
           src="https://dummyimage.com/720x600"
-        />
+        /> */}
+        <Image
+          src={banner}
+          alt="hero"
+          width="200"
+          height="300"
+          >
+
+          </Image>
         <div className="text-center lg:w-2/3 w-full">
-          <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
-            Microdosing synth tattooed vexillologist
+          <h1 className="title-font mt-5 sm:text-4xl text-3xl mb-4 font-bold text-gray-900">
+            FRONT END DEVELOPER
           </h1>
           <p className="mb-8 leading-relaxed">
-            Meggings kinfolk echo park stumptown DIY, kale chips beard jianbing
-            tousled. Chambray dreamcatcher trust fund, kitsch vice godard
-            disrupt ramps hexagon mustache umami snackwave tilde chillwave ugh.
-            Pour-over meditation PBR&B pickled ennui celiac mlkshk freegan photo
-            booth af fingerstache pitchfork.
+          As a Front-End Developer with experience in HTML, CSS, JavaScript, and various frameworks and libraries such as ReactJs, NextJs. I am dedicated to creating visually appealing and intuitive websites and applications.
           </p>
           <div className="flex justify-center">
+            <Link href='/projects'>
             <button className="inline-flex text-white bg-red-500 border-0 py-2 px-6 focus:outline-none hover:bg-red-600 rounded text-lg">
-              Button
+            My Work 
             </button>
-            <button className="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg">
-              Button
-            </button>
+            </Link>
           </div>
         </div>
       </div>
